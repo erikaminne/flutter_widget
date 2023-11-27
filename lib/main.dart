c7109c07b7b4ae541dba5c66fda908d803864769
Erik Aminne <erik.oskar.aminne@gmail.com>
2023-11-27 10:41:49 +0200

    update lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,45 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(
+      const MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('My third App'),
-          centerTitle: true,
-          backgroundColor: Colors.amber[200],
-        ),
-        body: const Center(
-          child: Text(
-            'We are doing this again',
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
+      appBar: AppBar(
+        title: const Text('My third App'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: const Center(
+        child: Text(
+          'We are doing this again',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            color: Colors.black,
           ),
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {SystemNavigator.pop()},
+        backgroundColor: Colors.red,
+        child: const Text(
+          'Exit',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
