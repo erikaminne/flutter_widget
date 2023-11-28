bfd529a609166ad3f87d2ac38c71f6c0e6d08e69
Erik Aminne <erik.oskar.aminne@gmail.com>
2023-11-28 12:53:30 +0200

    updating main.dart padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,23 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
-void main() => runApp(
-      const MaterialApp(
-        home: Home(),
-        debugShowCheckedModeBanner: false,
-      ),
-    );
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 0, 0),
       appBar: AppBar(
-        title: const Text('Buttons',
-            style: TextStyle(
-              color: Colors.white,
-            )),
-        centerTitle: true,
-        backgroundColor: Colors.black,
+        title: const Text('Padding'),
+        backgroundColor: Colors.greenAccent,
       ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-            print('You clicked me');
-          },
-          icon: const Icon(Icons.home, color: Color.fromARGB(255, 255, 0, 0)),
-          iconSize: 50,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.black,
-            foregroundColor: Color.fromARGB(255, 150, 0, 0),
-          ),
-        ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(45, 25, 75, 5),
+        margin: const EdgeInsets.all(150),
+        color: Colors.grey[600],
+        child: Text('Test Text'),
       ),
     );
   }
